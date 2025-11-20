3298754ab10f77eeac65b2c2fef19a87847babc5
DevarathN <devarathnediyirippil@gmail.com>
2025-11-20 20:04:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use("/api/tasks", taskRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log("❌ Error:", err));
 
